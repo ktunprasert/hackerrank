44dc277913033c1a65a039310d957ba3ca8d78d2
ktunprasert <ktunprasert@outlook.com>
2023-06-15 23:00:12 +0100

    python: two_strings

--- python/two_strings.py
+++ python/two_strings.py
@@ -1,21 +1,5 @@
 def twoStrings(s1, s2):
-    checked = {}
-    substr_len = 1
-    while substr_len < len(s1):
-        for j in range(len(s1) - substr_len + 1):
-            sub = s1[j : j + substr_len]
-            if sub in checked:
-                continue
-
-            checked[sub] = True
-
-            for k in range(len(s2) - substr_len + 1):
-                check_sub = s2[k : k + substr_len]
-                if sub == check_sub:
-                    return "YES"
-
-        substr_len += 1
-    return "NO"
+    return "YES" if len(set(s1) & set(s2)) else "NO"
 
 
 if __name__ == "__main__":
